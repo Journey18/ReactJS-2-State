ff3cf9b507391801d9dd9ab7296a44d58fef6e64
Journey18 <nghia.nguyentrung18@gmail.com>
2023-11-27 09:18:20 +0700

    update v1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GlassDisplay from './components/Glass/GlassDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <GlassDisplay/>
+    </div>
+  );
+}
+
+export default App;
--- state.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class state extends Component {
-  render() {
-    return <div>state</div>;
-  }
-}
